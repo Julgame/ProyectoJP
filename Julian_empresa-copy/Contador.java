ab1e25eef8e8278c724d9017eadd96148235dd0b
Jhonny Almonacid <klesvy123@gmail.com>
2023-06-20 08:46:55 -0500

    Primer commit

---
+++ Julian_empresa-copy/Contador.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Contador {
+    
+    public static void main(String[] args) {
+        int a;
+        double totalArreglos = 0;
+        double arreglo;
+        double prom;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite cuántos arreglos al mes:");
+        a = scanner.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            System.out.println("Ingrese el valor del arreglo " + i + ":");
+            arreglo = scanner.nextDouble();
+            totalArreglos += arreglo;
+        }
+
+        prom = totalArreglos / a;
+
+        System.out.println("El promedio total de arreglos del mes: " + prom);
+    }
+}
+
